931aa2f1c269ac6c1ec4fc0f0e7b9a1deaeb734f
yurifrl <yurifrl@hotmail.com>
2017-02-08 00:37:47 -0200

    remove noise

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -2,6 +2,6 @@ package main.java.com.app;
 
 public class App {
   public static void main( String[] args ) {
-    // System.out.println("Hello From Main !!");
+    System.out.println("Hello From Main !!");
   }
 }
